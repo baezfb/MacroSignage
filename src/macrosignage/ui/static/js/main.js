ecaa2b52eed1e8bb28027a6fb5edeeb63b2a772c
baezfb <baezdevs@gmail.com>
2022-10-08 15:22:28 -0500

    added open sign

--- src/macrosignage/ui/static/js/main.js
+++ src/macrosignage/ui/static/js/main.js
@@ -57,3 +57,17 @@ function autoPlayCarousel() {
 function init() {
     autoPlayCarousel();
 }
+
+const setProperty = (duration) => {
+    document.documentElement.style.setProperty(
+        "--animation-time",
+        duration + "s"
+    );
+};
+
+const changeAnimationTime = () => {
+    const animationDuration = Math.random();
+    setProperty(animationDuration);
+};
+
+setInterval(changeAnimationTime, 1000);
